92d1b4ff2eb64212b9d37436bace8277e1406531
Logan <105866075+RoundDoor@users.noreply.github.com>
2025-03-26 09:56:39 -0400

    Compile
    Made it compile on Win32

--- src/Lib.rs
+++ src/Lib.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+pub mod nt;
+pub mod rzctl;
+
+#[cfg(test)]
+mod tests {
+    use super::rzctl;
+
+    #[test]
+    fn test_mouse_move() {
+        // Initialize the device
+        if rzctl::init() {
+            // Move the mouse to (100, 100) from the current position
+            rzctl::mouse_move(100, 100, true);
+        } else {
+            panic!("Failed to initialize the device.");
+        }
+    }
 }
